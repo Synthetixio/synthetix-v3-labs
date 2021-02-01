61d1f388acd8085f21d2ae86f3fd330d1ff52db1
Alejandro <palebluedot@gmail.com>
2021-02-01 17:30:17 -0300

    Update comments

--- proxy-architecture-poc-4/contracts/Synthetix.sol
+++ proxy-architecture-poc-4/contracts/Synthetix.sol
@@ -17,15 +17,15 @@ contract Synthetix {
             msg.sig == 0x788bc78c || // setVersion(string)
             msg.sig == 0x430fe9c1 || // getDate()
             msg.sig == 0x03c0a389    // setDate()
-        ) implementation = 0xcdA522f72aC0Ab675f9d9b27686229B9EE60F064;
+        ) implementation = 0xcdA522f72aC0Ab675f9d9b27686229B9EE60F064; // SystemModule
         else if (
             msg.sig == 0xd72e0705 || // getOracleType()
             msg.sig == 0xe6dbd15d || // setOracleType()
             msg.sig == 0x10916f3b    // getVersionViaExchanger()
-        ) implementation = 0x26a79B946C58fDa1b23a2E88bFEfA7E2a8F7DAf8;
+        ) implementation = 0x26a79B946C58fDa1b23a2E88bFEfA7E2a8F7DAf8; // IssuerModule
         else if (
             msg.sig == 0xe017bb0d    // getSystemVersion()
-        ) implementation = 0x1f0A572FF4B2850D747be740728E7CBCE17755e9;
+        ) implementation = 0x1f0A572FF4B2850D747be740728E7CBCE17755e9; // ExchangerModule
         require(implementation != address(0), "Selector not registered in any module");
 
         // Delegatecall forwarder
