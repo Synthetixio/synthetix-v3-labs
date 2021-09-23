4c6f1b057f3665b1832a5aa87affe1fc41dbcebe
i-stam <i.stamelakos@gmail.com>
2021-09-23 19:54:42 +0100

    Implement dummy core functionality

--- debt-shares/contracts/DebtShares.sol
+++ debt-shares/contracts/DebtShares.sol
@@ -18,9 +18,14 @@ contract DebtShares is IDebtShares, ERC20("DebtShares", "dbt") {
         _burn(msg.sender, amount);    
     }
 
-    function stake(uint256 amount) external override {}
+    function stake(uint256 amount) external override {
+        _stake(amount);
+    }
 
-    function stakeAndIssue(uint256 stakeAmount, uint256 issueAmount) external override {}
+    function stakeAndIssue(uint256 stakeAmount, uint256 issueAmount) external override {
+        _stake(stakeAmount);
+        _issue(issueAmount);
+    }
 
     function issue(uint256 amount) external override {
         _issue(amount);
@@ -29,10 +34,23 @@ contract DebtShares is IDebtShares, ERC20("DebtShares", "dbt") {
     function maxIssuableSynths(address account) public view returns (uint256) {
         return type(uint).max;
     }
+
     // according to SIP-XXX the debt shares calulation is based on this formula: totalShares * (sUSDAmount / totalDebt)
     function _calculateSharesAmount(uint256 amount) internal view returns (uint) {
+        // if there is zero debt then return the amount requested
+        if (totalDebt == 0) {
+            return amount;
+        }
+        
+        return totalSupply() * amount / totalDebt;
+    }
 
-        return totalSupply() * totalDebt / amount;
+    function _stake(uint256 amount) internal {
+        // get th user's balance
+        uint256 balance = balanceOf(msg.sender);
+        // check if the balance is sufficient
+        require(amount <= balance, "Amount exceeds balance");
+         
     }
 
     function _issue(uint256 amount) internal {
@@ -40,11 +58,12 @@ contract DebtShares is IDebtShares, ERC20("DebtShares", "dbt") {
         uint256 issuable = maxIssuableSynths(msg.sender);
         // check if the requested amount is larger than the max
         require(amount <= issuable, "Amount too large");
-
-        uint256 totalDebtShares = totalSupply();
-        
-        _mint(msg.sender, amount);
-         
+        //calculate how many debt shares should be issued
+        uint256 debtSharesToBeIssued = _calculateSharesAmount(amount);
+        // mint the amount calculated in the previous step
+        _mint(msg.sender, debtSharesToBeIssued);
+        // TODO: issue sUSD
+        // issuesUSD();
     }
 
 }
